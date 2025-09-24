950503d756f7e13a5bda750e601a5877a209937e
CharlesLindberg <charleslindberg91@gmail.com>
2025-09-24 20:41:45 +0200

    L3- Added image imports

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { ref } from 'vue';
+import socksGreen from './assets/images/socks_green.jpeg';
+import socksBlue from './assets/images/socks_blue.jpeg';
+
 const product = ref('Socks');
+const image = ref(socksGreen);
+const image2 = ref(socksBlue);
 
 const description = ref('A pair of warm, fuzzy socks');
 </script>
@@ -8,6 +13,17 @@ const description = ref('A pair of warm, fuzzy socks');
 <template>
   <div class="product-display">
     <div class="product-container">
+      <div class="product-image">
+        <!-- image goes here -->
+        <img
+          :src="image"
+          alt="socks blue"
+        />
+        <img
+          :src="image2"
+          alt="socks blue"
+        />
+      </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
         <p>{{ description }}</p>
