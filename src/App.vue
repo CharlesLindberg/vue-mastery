2be72ca980f9365490ec329232eeb9901230119c
CharlesLindberg <charleslindberg91@gmail.com>
2025-09-25 11:18:11 +0200

    L4

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@ import socksBlue from './assets/images/socks_blue.jpeg';
 const product = ref('Socks');
 const image = ref(socksGreen);
 const image2 = ref(socksBlue);
+const inStock = ref(true);
+const inventory = ref(3);
+const onSale = ref(false);
 
 const description = ref('A pair of warm, fuzzy socks');
 </script>
@@ -27,6 +30,10 @@ const description = ref('A pair of warm, fuzzy socks');
       <div class="product-info">
         <h1>{{ product }}</h1>
         <p>{{ description }}</p>
+        <p v-if="inventory > 10">In Stock</p>
+        <p v-else-if="inventory <= 19 && inventory > 0">Almost sold out</p>
+        <p v-else>Out of Stock</p>
+        <p v-if="onSale">On Sale</p>
       </div>
     </div>
   </div>
