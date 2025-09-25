76b5deb0ec8ff55a58c75285c02425dfabccd1dc
CharlesLindberg <charleslindberg91@gmail.com>
2025-09-25 11:51:24 +0200

    L6

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,27 @@ const onSale = ref(false);
 const details = ['50% cotton', '30% wool', '20% polyester'];
 
 const variants = [
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
+  { id: 2234, color: 'green', image: socksGreen },
+  { id: 2235, color: 'blue', image: socksBlue },
 ];
 
 const sizes = ['S', 'M', 'L', 'XL'];
 
 const description = ref('A pair of warm, fuzzy socks');
+
+const cart = ref(0);
+const addToCart = () => (cart.value += 1);
+const removeFromCart = () => (cart.value -= 1);
+
+const updateImage = (variantImage) => {
+  image.value = variantImage;
+};
 </script>
 
 <template>
+  <div class="nav-bar"></div>
+  <div class="cart">{{ cart }}</div>
+
   <div class="product-display">
     <div class="product-container">
       <div class="product-image">
@@ -31,14 +42,10 @@ const description = ref('A pair of warm, fuzzy socks');
           :src="image"
           alt="socks blue"
         />
-        <img
-          :src="image2"
-          alt="socks blue"
-        />
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
-        <p>{{ description }}</p>
+        <!-- <p>{{ description }}</p> -->
         <p v-if="inventory > 10">In Stock</p>
         <p v-else-if="inventory <= 19 && inventory > 0">Almost sold out</p>
         <p v-else>Out of Stock</p>
@@ -49,9 +56,22 @@ const description = ref('A pair of warm, fuzzy socks');
         <div
           v-for="variant in variants"
           :key="variant.id"
+          @mouseover="updateImage(variant.image)"
         >
           {{ variant.color }}
         </div>
+        <button
+          class="button"
+          v-on:click="addToCart"
+        >
+          Add to Cart
+        </button>
+        <button
+          class="button"
+          @click="removeFromCart"
+        >
+          -
+        </button>
         <ul>
           <li v-for="size in sizes">{{ size }}</li>
         </ul>
